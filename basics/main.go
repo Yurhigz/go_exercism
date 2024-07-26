c336d315365dcf94bb4edb7d8879b0af5a80bc8a
Benoit.G <benoitgaugiran@gmail.com>
2024-07-26 22:07:08 +0200

    exercice answer

---
+++ basics/main.go
@@ -0,0 +1,16 @@
+package lasagna
+
+const OvenTime = 40
+
+func RemainingOvenTime(actual int) int {
+	return (OvenTime - actual)
+}
+
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return (OvenTime - RemainingOvenTime(actualMinutesInOven)) + PreparationTime(numberOfLayers)
+}
+
