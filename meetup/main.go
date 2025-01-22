a05d2a890c16735209f75b91ed42cc9d33a74fc4
Benoit.G <benoitgaugiran@gmail.com>
2025-01-22 23:19:42 +0100

    wip

--- meetup/main.go
+++ meetup/main.go
@@ -1,16 +1,28 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Define the WeekSchedule type here.
 
-type WekkSchedule struct {
-}
+type WeekSchedule int
+
+const (
+	First  WeekSchedule = 1
+	Second WeekSchedule = 8
+	Third  WeekSchedule = 15
+	Fourth WeekSchedule = 22
+	Teenth WeekSchedule = 13
+	Last   WeekSchedule = -6
+)
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	d := time.Date(year, month, int(wSched), 0, 0, 0, 0, time.UTC).Day()
+	return d
 }
 
 func main() {
-
+	fmt.Println(Day(Teenth, time.Monday, 8, 2013))
 }
