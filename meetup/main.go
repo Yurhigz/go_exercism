dbb994551e36ba0e761452d9cee1dda7156325ce
Benoit.G <benoitgaugiran@gmail.com>
2025-01-22 08:53:32 +0100

    start

---
+++ meetup/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+// Define the WeekSchedule type here.
+
+type WekkSchedule struct {
+}
+
+func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
+	panic("Please implement the Day function")
+}
+
+func main() {
+
+}
