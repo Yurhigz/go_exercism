78e2eae603980526db028542717e6b26c5bf9a7a
Benoit <90827157+Yurhigz@users.noreply.github.com>
2025-01-25 23:47:26 +0100

    Start

--- tournament/main.go
+++ tournament/main.go
@@ -1,26 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"strings"
 )
 
-func Tally(reader io.Reader, writer io.Writer) error {
-	fmt.Println(reader, writer)
+type performances struct {
+	team        string
+	matchPlayed int
+	wins        int
+	even        int
+	loss        int
+	totalPoint  int
+}
+
+func Tally(reader io.Reader) error {
+	r := bufio.NewReader(reader)
+	// data := make([]byte, 1000)
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 	return nil
 }
 
+// func Tally(reader io.Reader, writer io.Writer) error {
+// 	r := bufio.NewReader(reader)
+// 	for {
+
+// 	}
+// 	return nil
+// }
+
 func main() {
 
-	r := strings.NewReader(`Allegoric Alaskians;Blithering Badgers;win
+	// 	r := strings.NewReader(`Allegoric Alaskians;Blithering Badgers;win
+	// Devastating Donkeys;Courageous Californians;draw
+	// Devastating Donkeys;Allegoric Alaskians;win
+	// Courageous Californians;Blithering Badgers;loss
+	// Blithering Badgers;Devastating Donkeys;loss
+	// Allegoric Alaskians;Courageous Californians;win`)
+	// 	b, _ := Read(r)
+
+	// Exemple : un io.Reader simulé
+	data := `Allegoric Alaskians;Blithering Badgers;win
 Devastating Donkeys;Courageous Californians;draw
 Devastating Donkeys;Allegoric Alaskians;win
 Courageous Californians;Blithering Badgers;loss
 Blithering Badgers;Devastating Donkeys;loss
-Allegoric Alaskians;Courageous Californians;win`)
-	b, _ := io.ReadAll(r)
-
-	score := make(map[string][]int)
-
+Allegoric Alaskians;Courageous Californians;win`
+	Tally(strings.NewReader(data))
 }
