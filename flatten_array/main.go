acddab834668d784ae8508bf98a77efb7eadbb60
Benoit.G <benoitgaugiran@gmail.com>
2025-02-04 22:34:24 +0100

    Gotta solve the empty interface problem

--- flatten_array/main.go
+++ flatten_array/main.go
@@ -7,23 +7,32 @@ import (
 
 func Flatten(nested interface{}) []interface{} {
 	nest := reflect.ValueOf(nested)
+	if nest.Len() == 0 && nest.Kind() == reflect.Slice {
+		return []interface{}{}
+	}
+
 	var results []interface{}
+
 	if nest.Kind() == reflect.Array || nest.Kind() == reflect.Slice {
 		for j := 0; j < nest.Len(); j++ {
 			elem := nest.Index(j).Interface()
-			if reflect.ValueOf(elem).Kind() == reflect.Slice {
+			elemValue := reflect.ValueOf(elem)
+			if elemValue.Kind() == reflect.Slice && elemValue.Len() > 0 {
 				results = append(results, Flatten(elem)...)
-			} else {
+			} else if elem != nil {
 				results = append(results, elem)
 			}
-
 		}
-	} else {
+	} else if reflect.ValueOf(nested).Len() > 0 {
 		results = append(results, nested)
+	} else if reflect.ValueOf(results).Len() == 0 && reflect.ValueOf(nested).Len() == 0 {
+		return []interface{}{}
 	}
 	return results
 }
 
 func main() {
 	fmt.Println(Flatten([]interface{}{1, []interface{}{2, 3, 4, 5, 6, 7}, 8}))
+	fmt.Println(Flatten([]interface{}{1, 2, interface{}(nil)}))
+	fmt.Println(Flatten([]interface{}{[]interface{}{[]interface{}{}}}) == nil)
 }
