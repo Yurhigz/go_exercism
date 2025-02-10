58bed150e66d3909705165d9b6c01406438dfd3c
Benoit.G <benoitgaugiran@gmail.com>
2025-02-10 21:52:02 +0100

    update

--- parallel_letter_frequency/main.go
+++ parallel_letter_frequency/main.go
@@ -21,20 +21,20 @@ func Frequency(text string) FreqMap {
 // by making use of concurrency.
 func ConcurrentFrequency(texts []string) FreqMap {
 	c := make(chan FreqMap)
-
+	done := make(chan bool)
 	numGoroutines := len(texts)
 	FreqMap := make(FreqMap)
 
 	for _, element := range texts {
 		go func(t string) {
 			c <- Frequency(t)
-
+			done <- true
 		}(element)
 	}
 
 	go func() {
 		for i := 0; i < numGoroutines; i++ {
-
+			<-done // Attendre la fin d'une Goroutine
 		}
 		close(c)
 	}()
@@ -48,6 +48,22 @@ func ConcurrentFrequency(texts []string) FreqMap {
 		// }
 	}
 	return FreqMap
+}
+
+// ConcurrentFrequency concurrently counts the frequency of each rune in each item in a given list of texts
+// and returns this data as a FreqMap.
+func ConcurrentFrequency2(list []string) FreqMap {
+	ch := make(chan FreqMap)
+	for _, s := range list {
+		go func(s string) { ch <- Frequency(s) }(s)
+	}
+	result := FreqMap{}
+	for range list {
+		for letter, count := range <-ch {
+			result[letter] += count
+		}
+	}
+	return result
 }
 
 func main() {
