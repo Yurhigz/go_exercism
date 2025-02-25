54d4942c4fcfc40c6a9cf86f197ce8f48b0f56a4
Benoit.G <benoitgaugiran@gmail.com>
2025-02-25 22:50:44 +0100

    WIP almost done

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,23 +1,53 @@
 package matrix
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 // Define the Matrix type here.
 type Matrix struct {
 	rows    [][]int
 	columns [][]int
 }
 
+func isMatrix(s string) bool {
+	rows := strings.Split(s, "\n")
+	if len(rows) == 0 {
+		return false
+	}
+	var col int
+	for i, values := range rows {
+		nums := strings.Fields(values)
+		if i == 0 {
+			col = len(nums)
+		} else if len(nums) != col {
+			return false
+		}
+		for _, v := range nums {
+			_, err := strconv.Atoi(v)
+			if err != nil {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func New(s string) (Matrix, error) {
-	col := [][]int{}
-	row := [][]int{}
-	iteratorCol := 0
-	iteratorRow := 0
-	for value := range s {
-		if value == '\n' {
-			iteratorRow++
-		} else {
-			col[iteratorCol] = append(col[iteratorCol], int(value))
-			row[iteratorRow] = append(row[iteratorRow], int(value))
-			iteratorCol++
+	if !isMatrix(s) {
+		return Matrix{}, errors.New("argument must be a matrix")
+	}
+	rows := strings.Split(s, "\n")
+	row := make([][]int, len(rows))
+	col := make([][]int, len(strings.Fields(rows[0])))
+	for i, value := range rows {
+		nums := strings.Fields(value)
+		for k, v := range nums {
+			numbers, _ := strconv.Atoi(v)
+			row[i] = append(row[i], numbers)
+			col[k] = append(col[k], numbers)
 		}
 	}
 	return Matrix{rows: row, columns: col}, nil
